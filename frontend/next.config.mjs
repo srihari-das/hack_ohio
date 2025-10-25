c9fa1b3d391316b97a66d1ca799e04fe6c6b5b07
Dylan Yang <osuyang.6294@gmail.com>
2025-10-25 14:51:32 -0400

    connect frontend backend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*', // FastAPI backend
+      }
+    ]
+  },
 };
 
 export default nextConfig;
