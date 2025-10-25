b0ea5218df756969d287653553f15694a03f8ea1
Dylan Yang <osuyang.6294@gmail.com>
2025-10-25 16:45:56 -0400

    homepage background

---
+++ frontend/src/app/components/Homepage.js
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+
+export default function Homepage() {
+    return (
+        <div className="fixed inset-0 z-0">
+            <Image
+            src = "/ddb_home_background.jpg"
+            alt="Background Image"
+            fill
+            className="object-fill"
+            />
+        </div>
+    )
+}
